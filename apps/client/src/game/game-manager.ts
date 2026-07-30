66b6b9e2d8ec1fbb24962f2b8fd49f9e448bc105
matfij <mateusz.michal.fijak@gmail.com>
2026-07-30 22:11:35 +0200

    feat: base movement

--- apps/client/src/game/game-manager.ts
+++ apps/client/src/game/game-manager.ts
@@ -1,10 +1,14 @@
-import { Application, Container, Graphics, wordWrap } from "pixi.js";
+import { Application, Container, Graphics } from "pixi.js";
+
+import { InputManager } from "./input-manager";
+
+const speed = 300;
 
 export class GameManager {
     private initialized = false;
     readonly app = new Application();
     readonly map = new Container();
-    player = { x: 200, y: 200 };
+    player = { ship: new Graphics(), x: 200, y: 200, r: 0 };
 
     async initialize(container: HTMLElement) {
         await this.app.init({
@@ -12,30 +16,65 @@ export class GameManager {
             background: "#000814",
             antialias: true,
         });
+
+        InputManager.initialize();
+
         container.replaceChildren();
+
         this.app.stage.addChild(this.map);
+
         container.appendChild(this.app.canvas);
+
+        this.app.ticker.add((ticker) => {
+            this.update(ticker.deltaTime / 60);
+        });
+
+        // dummy asteroid
+        this.map.addChild(new Graphics().circle(600, 300, 30).fill(0xff0000));
+
         this.initialized = true;
     }
 
     addShip() {
-        const ship = new Graphics()
-            .moveTo(this.player.x, this.player.y)
-            .lineTo(this.player.x - 10, this.player.y - 10)
-            .lineTo(this.player.x - 5, this.player.y)
-            .lineTo(this.player.x - 10, this.player.y + 10)
+        this.player.ship = new Graphics()
+            .moveTo(20, 0)
+            .lineTo(-10, -10)
+            .lineTo(-5, 0)
+            .lineTo(-10, 10)
             .closePath()
-            .stroke({ color: 0x00ffff, width: 2 });
+            .stroke({
+                color: 0x00ffff,
+                width: 2,
+            });
 
-        ship.position.set(0, 0);
+        this.player.ship.position.set(this.player.x, this.player.y);
 
-        this.map.addChild(ship);
+        this.map.addChild(this.player.ship);
+    }
+
+    update(dt: number) {
+        if (InputManager.isPressed("KeyW")) {
+            this.player.y -= speed * dt;
+        }
+        if (InputManager.isPressed("KeyS")) {
+            this.player.y += speed * dt;
+        }
+        if (InputManager.isPressed("KeyA")) {
+            this.player.x -= speed * dt;
+        }
+        if (InputManager.isPressed("KeyD")) {
+            this.player.x += speed * dt;
+        }
+
+        this.player.ship.position.set(this.player.x, this.player.y);
+
+        this.camera();
     }
 
     camera() {
         this.map.position.set(
             this.app.screen.width / 2 - this.player.x,
-            this.app.screen.width / 2 - this.player.y,
+            this.app.screen.height / 2 - this.player.y,
         );
     }
 
