f9ee248402146687e1c1ec69b6c41e5466d33909
matfij <mateusz.michal.fijak@gmail.com>
2026-08-15 17:45:41 +0200

    refactor: server config

--- packages/shared/src/config.ts
+++ packages/shared/src/config.ts
@@ -1,6 +1,10 @@
 export const gameConfig = {
     dt: 50 / 1000, // game tick
-    inactiveThreshold: 15_000, // ms inactivity threshold before disconnect
     playerRespawnThreshold: 5, // seconds to respawn
-    serverPort: 3000,
 } as const;
+
+export const serverConfig = {
+    port: 3000,
+    maxMessageSize: 2 * 1024, // 2 Kib,
+    inactiveThreshold: 15_000, // ms inactivity threshold before disconnect
+};
