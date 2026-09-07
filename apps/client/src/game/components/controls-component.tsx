b7201a0be182517e068987eb20ebb1be2905b1d0
matfij <mateusz.michal.fijak@gmail.com>
2026-09-07 20:16:08 +0200

    feat: joystick movement

--- apps/client/src/game/components/controls-component.tsx
+++ apps/client/src/game/components/controls-component.tsx
@@ -1,16 +1,59 @@
-import { useState } from "react";
+import { useState, type PointerEvent } from "react";
 
 import styles from "../game-component.module.scss";
 import controlsStyles from "./controls-component.module.scss";
 
+const config = {
+    dotSize: 24,
+    joystickRadius: 48,
+};
+
 export const ControlsComponent = () => {
+    const [position, setPosition] = useState({ x: 40, y: 40 });
     const [showControls, setShowControls] = useState(false);
+
+    const onPointerDown = (event: PointerEvent) => {
+        event.currentTarget.setPointerCapture(event.pointerId);
+        updatePosition(event);
+    };
+
+    const onPointerMove = (event: PointerEvent) => {
+        if (!event.currentTarget.hasPointerCapture(event.pointerId)) {
+            return;
+        }
+        updatePosition(event);
+    };
+
+    const updatePosition = (event: PointerEvent) => {
+        const bounds = event.currentTarget.getBoundingClientRect();
+        const center = bounds.width / 2;
+        const pointerX = event.clientX - bounds.left;
+        const pointerY = event.clientY - bounds.top;
+        const vectorX = pointerX - center;
+        const vectorY = pointerY - center;
+        const distance = Math.hypot(vectorX, vectorY);
+        const scale = distance > config.joystickRadius ? config.joystickRadius / distance : 1;
+        const clampedX = vectorX * scale;
+        const clampedY = vectorY * scale;
+
+        setPosition({
+            x: center - config.dotSize + clampedX,
+            y: center - config.dotSize + clampedY,
+        });
+    };
 
     return (
         <>
             {showControls && (
-                <div className={`${styles.dialogWrapper} ${controlsStyles.joystickWrapper}`}>
-                    <div className={controlsStyles.joystickItem} />
+                <div
+                    onPointerDown={onPointerDown}
+                    onPointerMove={onPointerMove}
+                    className={`${styles.dialogWrapper} ${controlsStyles.joystickWrapper}`}
+                >
+                    <div
+                        className={controlsStyles.joystickItem}
+                        style={{ transform: `translate(${position.x}px, ${position.y}px)` }}
+                    />
                 </div>
             )}
 
